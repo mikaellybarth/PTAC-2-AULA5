965f9385049e3f756b5fd90e29dcbf5d8de3b225
mikaellybarth <wwwmikasouza06@gmail.com>
2023-09-15 14:32:59 -0400

    TRABALHO DO MARCELINO

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,31 +2,48 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
-   const [atividade, setAtividade ] = useState("");
+   const [produto, setProduto ] = useState("");
+   const [marca, setMarca ] = useState("");
+   const [preco, setPreco ] = useState("");
    const [lista, setLista ] = useState([]);
    const [id,setId] = useState(1);
     const salvar =(e) =>{
         e.preventDefault();
         setLista([...lista, {
-                atividade: atividade,
+                produto: produto, marca:marca, preco:preco,
                 id: id
         }]);
         setId(id + 1);
-        setAtividade("");
+        setProduto("");
+        setMarca("");
+        setPreco("");
     };
 
     return (
         <div>
             <Link to="/">home</Link>
-            <h1>Lista de Atividades</h1>    
+            <h1>Produtos Petshop</h1>    
             <form onSubmit={salvar}>
-            <input value={atividade} type="text"
-            onChange={(e)=>{ setAtividade(e.target.value)}}/>
+           
+            <p>Produto:</p>
+            <input value={produto} type="text"
+            onChange={(e)=>{ setProduto(e.target.value)}}/>
+            
+           <p>Marca do produto:</p>
+           <input value={marca} type="text"
+            onChange={(e)=>{ setMarca(e.target.value)}}/>
+           
+           <p>Valor do produto:</p>
+           <input value={preco} type="number"
+            onChange={(e)=>{ setPreco(e.target.value)}}/>
+
             <button>ADD</button>   
             </form>      
             {lista.map((ativ)=>
             <div key= {ativ.id}>
-                <p>{ativ.atividade}</p>
+                <p>Produto:{ativ.produto},
+                 Marca:{ativ.marca}, 
+                 R$:{ativ.preco}</p>
             </div>
             )} 
         </div>
