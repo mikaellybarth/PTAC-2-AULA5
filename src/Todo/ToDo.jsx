ee1b9805f575e30d4e5117d3dbd78cba8edd841c
mikaellybarth <wwwmikasouza06@gmail.com>
2023-09-20 10:03:41 -0400

    comittar

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -5,22 +5,24 @@ export default function ToDo() {
    const [produto, setProduto ] = useState("");
    const [marca, setMarca ] = useState("");
    const [preco, setPreco ] = useState("");
+   const [img, setImg ] = useState("");
    const [lista, setLista ] = useState([]);
    const [id,setId] = useState(1);
     const salvar =(e) =>{
         e.preventDefault();
         setLista([...lista, {
-                produto: produto, marca:marca, preco:preco,
+                produto: produto, marca:marca, preco:preco, img: img,
                 id: id
         }]);
         setId(id + 1);
         setProduto("");
         setMarca("");
         setPreco("");
+        setImg("");
     };
 
     return (
-        <div>
+        <div class = "center">
             <Link to="/">home</Link>
             <h1>Produtos Petshop</h1>    
             <form onSubmit={salvar}>
@@ -37,10 +39,15 @@ export default function ToDo() {
            <input value={preco} type="number"
             onChange={(e)=>{ setPreco(e.target.value)}}/>
 
+           <p>img:</p>
+           <input value={img} type="text"
+            onChange={(e)=>{ setImg(e.target.value)}}/>
+
             <button>ADD</button>   
             </form>      
             {lista.map((ativ)=>
             <div key= {ativ.id}>
+                <img src= {ativ.img}alt="" class = "img" />
                 <p>Produto:{ativ.produto},
                  Marca:{ativ.marca}, 
                  R$:{ativ.preco}</p>
