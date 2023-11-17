79417ed9b76c0d5799234ddd08af05cf1e4c8184
mikaellybarth <wwwmikasouza06@gmail.com>
2023-11-17 14:09:54 -0400

    commit nao sei se ta certo ta sem net

--- src/Detalhe/detalhe.jsx
+++ src/Detalhe/detalhe.jsx
@@ -1,19 +1,22 @@
 import { useParams } from "react-router-dom"
+import Card from '../Componentes/card';
 
 export default function detalhe (){
     const { id } = useParams();
     const lista = JSON.parse(localStorage.getItem("Lista"));
     
-    lista.filter((objeto)=>{
+   const produto = lista.filter((objeto)=>{
         if(objeto.id == id){
                   return objeto;
         }
         return null;
     })
 
+    console.log(produto[0])
+
     return(
         <div>
-            <h1>{id} bolsonaro presidente e o resto fia da puta,... Eu tomei um prejuizo com o bolsonaro de...</h1>
+           <Card produto={produto[0] } />
         </div>
     )
 } 
